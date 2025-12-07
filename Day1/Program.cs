e0cabe2029ee86f78f87dbfb919cbd67f4453314
Ioan-Gabriel Camen <i.camen@deltapi.nl>
2025-12-07 23:22:42 +0200

    Solved first problem of Day1, solved first case of second problem of Day1

---
+++ Day1/Program.cs
@@ -0,0 +1,13 @@
+﻿using Day1;
+
+var secretEntrance = new SecretEntrance();
+Console.WriteLine("First challenge:");
+var password = secretEntrance.GetPassword("TestInput.txt", true); // correct answer 3
+Console.WriteLine(password);
+password = secretEntrance.GetPassword("RealInput.txt", true); // correct answer 1052
+Console.WriteLine(password);
+Console.WriteLine("Second challenge:");
+password = secretEntrance.GetPassword("TestInput.txt", false); // correct answer 6
+Console.WriteLine(password);
+password = secretEntrance.GetPassword("RealInput.txt", false); // correct answer 6295
+Console.WriteLine(password);
---
+++ Day1/SecretEntrance.cs
@@ -0,0 +1,60 @@
+﻿namespace Day1;
+
+public class SecretEntrance
+{
+    #region Public members
+    public int GetPassword(string inputFileName, bool firstChallenge)
+    {
+        const int initialDialPosition = 50;
+        const int numberOfDials = 100;
+        const int correctDialPosition = 0;
+        var currentDialPosition = initialDialPosition;
+        var rotations = File.ReadAllLines(Path.Combine(AppContext.BaseDirectory, inputFileName)).ToList();
+        var counterOfCorrectDials = 0;
+        foreach (var rotation in rotations)
+        {
+            var previousPosition = currentDialPosition;
+            var turnDirection = rotation.First();
+            var turnNumber = int.Parse(rotation[1..]);
+            var goneThru = false;
+            if (turnDirection == 'L')
+            {
+                currentDialPosition -= turnNumber;
+                while (currentDialPosition < 0)
+                {
+                    currentDialPosition += numberOfDials;
+                    if (firstChallenge)
+                        continue;
+                    if (previousPosition != 0)
+                    {
+                        counterOfCorrectDials++;
+                    }
+                    previousPosition = currentDialPosition;
+                    goneThru = true;
+                }
+            }
+            else
+            {
+                currentDialPosition += turnNumber;
+                while (currentDialPosition >= numberOfDials)
+                {
+                    currentDialPosition -= numberOfDials;
+                    if (firstChallenge)
+                        continue;
+                    if (previousPosition != 0)
+                    {
+                        counterOfCorrectDials++;
+                    }
+                    previousPosition = currentDialPosition;
+                    goneThru = true;
+                }
+            }
+            
+            if (!goneThru && currentDialPosition == correctDialPosition)
+                counterOfCorrectDials++;
+        }
+
+        return counterOfCorrectDials;
+    }
+    #endregion
+}
